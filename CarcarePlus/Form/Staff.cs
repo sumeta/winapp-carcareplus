1b002cf626e7074be6b4d073f617b09d270988c3
top_s <top_s@LAPTOP-N826J3CQ>
2020-09-17 21:49:03 +0700

    Data Grid

--- CarcarePlus/Form/Staff.cs
+++ CarcarePlus/Form/Staff.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Data.SQLite;
+using System.Data.SqlClient;
 
 namespace CarcarePlus
 {
@@ -35,6 +36,79 @@ namespace CarcarePlus
             {
                 Console.WriteLine($"{rdr.GetInt32(0)} {rdr.GetString(1)} {rdr.GetString(2)}");
             }
+
+
+            SQLiteCommand comm = new SQLiteCommand("Select * From staff", con);
+            using (SQLiteDataReader read = comm.ExecuteReader())
+            {
+                while (read.Read())
+                {
+                    dataGridView1.Rows.Add(new object[] {
+                //read.GetValue(0),  // U can use column index
+                read.GetValue(read.GetOrdinal("Id")),  // Or column name like this
+                read.GetValue(read.GetOrdinal("Name")),
+                read.GetValue(read.GetOrdinal("Tel"))
+            });
+                }
+            }
+
+
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+            if (dataGridView1.CurrentCell.ColumnIndex >= 0  && e.RowIndex != -1)
+            {
+                if (dataGridView1.CurrentCell != null && dataGridView1.CurrentCell.Value != null)
+                {
+                    textBox1.Text = dataGridView1.CurrentCell.Value.ToString();
+                    textBox2.Text = dataGridView1.CurrentRow.Cells["Tel"].Value.ToString();
+                }
+            }
+        }
+
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(dataGridView1.CurrentCell.Value.ToString());
+
+        }
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("xxxx");
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void s(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("OK");
         }
     }
 }
