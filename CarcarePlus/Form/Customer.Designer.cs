b3b3f66ac184a10304685f6b1b86f8b5361b85d6
tpsumeta <tpsumeta@gmail.com>
2020-09-27 17:06:20 +0700

    customer edit

--- CarcarePlus/Form/Customer.Designer.cs
+++ CarcarePlus/Form/Customer.Designer.cs
@@ -32,56 +32,115 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.Code = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Firstname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Lastname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Line = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(42, 64);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Code,
+            this.Firstname,
+            this.Lastname,
+            this.Tel,
+            this.Line});
+            this.dataGridView1.Location = new System.Drawing.Point(56, 79);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(707, 267);
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(943, 346);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(42, 350);
+            this.button1.Location = new System.Drawing.Point(56, 455);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 43);
+            this.button1.Size = new System.Drawing.Size(149, 53);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "เพิ่ม";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(211, 350);
+            this.button2.Location = new System.Drawing.Point(281, 455);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 43);
+            this.button2.Size = new System.Drawing.Size(149, 53);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "แก้ไข";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(384, 350);
+            this.button3.Location = new System.Drawing.Point(850, 455);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(112, 43);
+            this.button3.Size = new System.Drawing.Size(149, 53);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "ลบ";
             this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Code
+            // 
+            this.Code.HeaderText = "Code";
+            this.Code.MinimumWidth = 6;
+            this.Code.Name = "Code";
+            this.Code.Width = 125;
+            // 
+            // Firstname
+            // 
+            this.Firstname.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Firstname.HeaderText = "Firstname";
+            this.Firstname.MinimumWidth = 6;
+            this.Firstname.Name = "Firstname";
+            // 
+            // Lastname
+            // 
+            this.Lastname.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Lastname.HeaderText = "Lastname";
+            this.Lastname.MinimumWidth = 6;
+            this.Lastname.Name = "Lastname";
+            // 
+            // Tel
+            // 
+            this.Tel.HeaderText = "Tel";
+            this.Tel.MinimumWidth = 6;
+            this.Tel.Name = "Tel";
+            this.Tel.Width = 125;
+            // 
+            // Line
+            // 
+            this.Line.HeaderText = "Line";
+            this.Line.MinimumWidth = 6;
+            this.Line.Name = "Line";
+            this.Line.Width = 125;
             // 
             // Customer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Customer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "ลูกค้า";
+            this.Load += new System.EventHandler(this.Customer_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -93,5 +152,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Code;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Firstname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Lastname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Line;
     }
 }
--- CarcarePlus/Form/Customer.cs
+++ CarcarePlus/Form/Customer.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Data.SQLite;
 using System.Windows.Forms;
 
 namespace CarcarePlus
@@ -19,6 +20,43 @@ namespace CarcarePlus
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void Customer_Load(object sender, EventArgs e)
+        {
+            load_data();
+        }
+
+
+        private void load_data()
+        {
+            var db = new Db();
+            var con = db.connect();
+
+            SQLiteCommand comm = new SQLiteCommand("select * from customer", con);
+            using (SQLiteDataReader read = comm.ExecuteReader())
+            {
+                dataGridView1.Rows.Clear();
+                while (read.Read())
+                {
+                    dataGridView1.Rows.Add(new object[] {
+                        read.GetValue(read.GetOrdinal("Code")),
+                        read.GetValue(read.GetOrdinal("Firstname")),
+                        read.GetValue(read.GetOrdinal("Lastname")),
+                        read.GetValue(read.GetOrdinal("Tel"))
+                    });
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new CustomerEdit().ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new CustomerEdit().ShowDialog();
         }
     }
 }
